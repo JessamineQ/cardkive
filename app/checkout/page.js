5bb6b90b979abfbcfaf42d444a3f1c78968041e6
JessamineQ <jessamine.qu0@gmail.com>
2025-01-04 17:12:07 -0600

    cart & checkout

---
+++ app/checkout/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout"
+
+export default function Checkout() {
+    return (
+        <>
+            <MainLayout>
+                <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
+                </div>
+            </MainLayout>
+        </>
+    )
+}
